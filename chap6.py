41a3f7b0aa88e068b00be3cc1d88191bbc266500
mikekrieger <mlkrgr@gmail.com>
2013-08-14 20:55:45 -0400

    week 2 files

--- chap6.py
+++ chap6.py
@@ -1,9 +1,51 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Michael Krieger
 
 
 # Ex. 6.6
 
+def first (word):
+	return word[0]
+
+def last (word):
+	return word[-1]
+
+def middle (word):
+	return word[1:-1]
 
 
-# Ex 6.8
+>>> middle('ab')
+''
+>>> middle('a')
+''
+>>> middle(' ')
+''
+
+
+
+
+def first (word):
+	return word[0]
+
+def last (word):
+	return word[-1]
+
+def middle (word):
+	return word[1:-1]
+
+def is_palindrome(word):
+	if len(word) <= 1:
+		return True
+	if first(word) != last(word):
+		return False
+	return is_palindrome(middle(word))
+
+
+	# Ex. 6.7
+import math
+def is_power(a, b):
+	if (b) == 1 and (a) !=1: return False
+	if (b) == 1 and (a) ==1: return True
+	if (b) == 0 and (a) !=1: return False
+	power = int (math.log (a, b) +0.5)
+	return b ** power == a
