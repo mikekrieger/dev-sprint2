0bf38d1dcd65d6a40b45a6a51e036092eff715da
mikekrieger <mlkrgr@gmail.com>
2013-08-16 16:08:57 -0400

    my_app

--- new_app/my_app.py
+++ new_app/my_app.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 
 app.secret_key = "apple"
 
-users = ('mike' 'apple')
+users = {'mike': 'apple'}
 
 class Main (flask.views.MethodView):
 	def get (self):
@@ -20,7 +20,7 @@ class Main (flask.views.MethodView):
 		for r in required:
 				if r not in flask.request.form:
 					flask.flash("error: Invalid login information. (0) is required.".format(r))
-					return flask.redirect(flask.url_for('index'))
+					return flask.redirect(flask.url_for('index.html'))
 		username = flask.request.form['username']
 		passwd = flask.request.form['passwd']
 		if username in users and users[username] == passwd:
@@ -40,10 +40,9 @@ def login_required(method):
 	return wrapper
 
 
-class View(flask.views.MethodView):
-	@login_required
+class Remote(flask.views.MethodView):
 	def get(self):
-		return flask.render_template('index.html')
+		return flask.render_template('remote.html')
 
 	@login_required
 	def post(self):
@@ -52,9 +51,12 @@ class View(flask.views.MethodView):
 		return flask.redirect(flask.url_for('remote'))
 		
 
-app.add_url_rule('/', 
-				view_func=View.as_view('main'), 
-				methods=['GET', 'POST'])
+app.add_url_rule('/',
+	view_func=Main.as_view('index'),
+	methods=["GET", "POST"])
+app.add_url_rule('/remote/',
+	view_func=Remote.as_view('remote'),
+	methods=['GET', 'POST'])
 
 app.debug = True
 app.run()
